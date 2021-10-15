b5a91d3518db4223f83bbdda20f3d6d3a0710001
Βασίλης Ψαρομάτης PC TECH GR EU BIILMAN <77292453+Bimanpc@users.noreply.github.com>
2021-10-15 19:12:23 +0300

    Add files via upload
    New Apps October 2021

---
+++ PCTeam.ps1
@@ -0,0 +1,28 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+[System.Windows.Forms.Application]::EnableVisualStyles()
+
+$form = New-Object System.Windows.Forms.Form
+$form.Text = "Some form"
+$form.Size = New-Object System.Drawing.Size(150, 145)
+$form.AutoSize = $true
+
+$lbl1 = New-Object System.Windows.Forms.Label
+$lbl1.Text = "Hello PC GEEK TEAM BY PCTECH  GR EU NEAPOLI LAKONIAS TEAM!"
+$lbl1.Location = New-Object System.Drawing.Point(30, 20);
+
+$lbl1.Add_Click({
+    [System.Windows.Forms.MessageBox]::Show("Hey!")
+})
+
+$btn = New-Object System.Windows.Forms.Button
+$btn.Text = "OK!!"
+$btn.location = New-Object System.Drawing.Point(30, 60);
+$btn.DialogResult = [System.Windows.Forms.DialogResult]::OK
+
+$form.AcceptButton = $btn
+$form.controls.Add($lbl1)
+$form.controls.Add($btn)
+
+$Null = $form.ShowDialog()
---
+++ pythagoras.py
@@ -0,0 +1,9 @@
+# PythagoraS The Formula
+import math
+# a.sq + b.sq = c.sq
+
+a = float(input('Value for A : '))
+b = float(input('Value for B : '))
+c = math.sqrt(a ** 2 + b ** 2)
+# Now the answer
+print(c)
