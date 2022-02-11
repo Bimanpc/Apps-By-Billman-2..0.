42a93235011c14c53b01092f3b458ca9b84797d6
Βασίλης Ψαρομάτης PC TECH GR EU BIILMAN <77292453+Bimanpc@users.noreply.github.com>
2022-02-11 19:36:05 +0200

    Add files via upload

---
+++ CPUHtml.ps1
@@ -0,0 +1,9 @@
+﻿write-host
+write-host "ShowCpuH.ps1,  Version 2022"
+write-host "Show CPU details in HTML5"
+write-host "Written by Billman PS"
+write-host "pctechgreu.rf.gd"
+write-host
+
+get-wmiobject -class Win32_Processor | convertto-html DeviceID,Name,CurrentClockSpeed,LoadPercentage -head "<title>CPU details for $env:computername</title>`n<style type=`"text/css`">`nbody { padding: 8px; line-height: 1.33 }`ntable { border-style: ridge }`ntd, th { padding: 10px; border-style: dotted; border-width: 1px }`nth { font-weight: bolder; text-align: center }`n</style>" | out-file -FilePath "showcpuh.html" -Encoding "ASCII"
+invoke-item "showcpuh.html"
---
+++ which.ps1
@@ -0,0 +1,12 @@
+﻿# Which.ps1,  Version 1.00
+# Locate the specified program file
+# Port of Unix' WHICH command
+#
+# Usage:  WHICH.PS1  filename[.ext]
+#
+# Where:  "filename" is a file name only: no drive or path
+#
+# Written by Vasils PS
+
+param([string] $file=$(throw "Please specify a filename."))
+(get-command $file).Definition
