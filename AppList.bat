31ee4eb16b830dfe720d9ad62ccbf7433c873662
Βασίλης Ψαρομάτης PC TECH GR EU BIILMAN <77292453+Bimanpc@users.noreply.github.com>
2022-06-10 19:38:28 +0300

    Add files via upload

---
+++ AppList.bat
@@ -0,0 +1,4 @@
+@echo off 
+Rem This is a list down all the files in the directory Program files 
+dir "C:\Program Files" > C:\lists.txt 
+echo "The program has completed"
---
+++ StringStar.bat
@@ -0,0 +1,6 @@
+@echo off 
+set str = The PCTHRGEU IS SUPERR!
+echo %str% 
+
+set str=%str:=% 
+echo %str%
